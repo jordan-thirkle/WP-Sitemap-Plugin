a7a39c547bf3244b9f3e7de07565081aa4e11fbd
Jordan Thirkle <jordantthirkle@gmail.com>
2023-01-09 18:49:50 +0000

    Create sitemap.php

---
+++ inc/sitemap.php
@@ -0,0 +1,20 @@
+<?php
+
+function generate_sitemap() {
+    $posts = wp_get_archives(array(
+        'type' => 'postbypost',
+        'limit' => -1,
+        'echo' => 0
+    ));
+    $pages = wp_get_archives(array(
+        'type' => 'page',
+        'limit' => -1,
+        'echo' => 0
+    ));
+    $sitemap = "<?xml version='1.0' encoding='UTF-8'?>\n";
+    $sitemap .= "<urlset xmlns='http://www.sitemaps.org/schemas/sitemap/0.9'>\n";
+    $sitemap .= $posts;
+    $sitemap .= $pages;
+    $sitemap .= "</urlset>\n";
+    return $sitemap;
+}
