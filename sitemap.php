b0b9bbde100f13b43dd241cbc7e68cfb1614a1c5
Jordan Thirkle <jordantthirkle@gmail.com>
2023-01-09 17:28:52 +0000

    Update sitemap.php
    This code does the following:
    
    Defines a constant called SITEMAP_PLUGIN_OPTIONS that is used to store the plugin's options in the database.
    
    Registers a new rewrite rule for the sitemap.xml file, so that it can be accessed at a custom URL (e.g. http://example.com/sitemap.xml).
    
    Adds a custom query variable called "sitemap" that is used to trigger the generation of the sitemap.
    
    Generates the sitemap and outputs it as an XML file when the "sitemap" query variable is set.
    
    Pings search engines like Google and Bing when a post or page is published or updated, to let them know that the sitemap has been updated. This behavior can be turned on or off using the settings page.
    
    Adds a link to the settings page in the plugin list.
    
    Registers the settings page, which allows the user to turn on or off the search engine pinging and footer link features.
    
    Saves the plugin's options to the database when the settings page is submitted.
    
    Flushes the rewrite rules when the plugin is activated or deactivated, to ensure that the sitemap.xml file is accessible.

--- sitemap.php
+++ sitemap.php
@@ -9,6 +9,8 @@
  * License: GPL2
  */
 
+define('SITEMAP_PLUGIN_OPTIONS', 'sitemap_plugin_options');
+
 function sitemap_plugin_init() {
     // Register a new rewrite rule for the sitemap.xml file
     add_rewrite_rule('sitemap\.xml$', 'index.php?sitemap=1', 'top');
@@ -22,6 +24,12 @@ function sitemap_plugin_init() {
     // Ping search engines when a post is published or updated
     add_action('publish_post', 'sitemap_plugin_ping_search_engines');
     add_action('publish_page', 'sitemap_plugin_ping_search_engines');
+
+    // Add a settings link to the plugin list
+    add_filter('plugin_action_links_' . plugin_basename(__FILE__), 'sitemap_plugin_settings_link');
+
+    // Register the settings page
+    add_action('admin_menu', 'sitemap_plugin_settings_page');
 }
 add_action('init', 'sitemap_plugin_init');
 
@@ -57,20 +65,95 @@ function generate_sitemap() {
     $sitemap .= "</urlset>\n";
     return $sitemap;
 }
-
 function sitemap_plugin_ping_search_engines() {
-    $sitemap_url = home_url('sitemap.xml');
-    $search_engines = array(
-        'http://www.google.com/webmasters/sitemaps/ping?sitemap=',
-        'http://www.bing.com/webmaster/ping.aspx?siteMap=',
-    );
-    foreach ($search_engines as $search_engine) {
-        wp_remote_get($search_engine . $sitemap_url);
+    $options = get_option(SITEMAP_PLUGIN_OPTIONS);
+    if ($options['ping_search_engines']) {
+        $sitemap_url = home_url('sitemap.xml');
+        $search_engines = array(
+            'http://www.google.com/webmasters/sitemaps/ping?sitemap=',
+            'http://www.bing.com/webmaster/ping.aspx?siteMap=',
+        );
+        foreach ($search_engines as $search_engine) {
+            wp_remote_get($search_engine . $sitemap_url);
+        }
     }
 }
 
 function sitemap_plugin_footer_link() {
-    $sitemap_url = home_url('sitemap.xml');
-    echo '<a href="' . $sitemap_url . '">Sitemap</a>';
+    $options = get_option(SITEMAP_PLUGIN_OPTIONS);
+    if ($options['show_footer_link']) {
+        $sitemap_url = home_url('sitemap.xml');
+        echo '<a href="' . $sitemap_url . '">Sitemap</a>';
+    }
 }
 add_action('wp_footer', 'sitemap_plugin_footer_link');
+
+function sitemap_plugin_settings_link($links) {
+    $settings_link = '<a href="options-general.php?page=sitemap-plugin">Settings</a>';
+    array_unshift($links, $settings_link);
+    return $links;
+}
+
+function sitemap_plugin_settings_page() {
+    add_options_page(
+        'Sitemap Plugin Settings',
+        'Sitemap Plugin',
+        'manage_options',
+        'sitemap-plugin',
+        'sitemap_plugin_settings_page_html'
+    );
+}
+
+function sitemap_plugin_settings_page_html() {
+    if (!current_user_can('manage_options')) {
+        wp_die('You do not have sufficient permissions to access this page.');
+    }
+    $options = get_option(SITEMAP_PLUGIN_OPTIONS);
+    if (isset($_POST['submit'])) {
+        $options['ping_search_engines'] = isset($_POST['ping_search_engines']);
+        $options['show_footer_link'] = isset($_POST['show_footer_link']);
+        update_option(SITEMAP_PLUGIN_OPTIONS, $options);
+    }
+    ?>
+    <div class="wrap">
+        <h1>Sitemap Plugin Settings</h1>
+        <form method="post">
+            <table class="form-table">
+                <tr>
+                    <th scope="row"><label for="ping_search_engines">
+Ping search engines</label></th>
+                    <td>
+                        <input type="checkbox" id="ping_search_engines" name="ping_search_engines" value="1" <?php checked($options['ping_search_engines']) ?> />
+                        <p class="description">Ping search engines (e.g. Google and Bing) when a post or page is published or updated, to let them know that the sitemap has been updated.</p>
+                    </td>
+                </tr>
+                <tr>
+                    <th scope="row"><label for="show_footer_link">Show footer link</label></th>
+                    <td>
+                        <input type="checkbox" id="show_footer_link" name="show_footer_link" value="1" <?php checked($options['show_footer_link']) ?> />
+                        <p class="description">Show a link to the sitemap in the footer of the page.</p>
+                    </td>
+                </tr>
+            </table>
+            <p class="submit">
+                <input type="submit" name="submit" id="submit" class="button button-primary" value="Save Changes">
+            </p>
+        </form>
+    </div>
+    <?php
+}
+
+register_activation_hook(__FILE__, 'sitemap_plugin_activate');
+function sitemap_plugin_activate() {
+    add_option(SITEMAP_PLUGIN_OPTIONS, array(
+        'ping_search_engines' => 1,
+        'show_footer_link' => 1,
+    ));
+    flush_rewrite_rules();
+}
+
+register_deactivation_hook(__
+FILE, 'sitemap_plugin_deactivate');
+function sitemap_plugin_deactivate() {
+    flush_rewrite_rules();
+}
